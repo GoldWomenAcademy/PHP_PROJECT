b4b89fdfe42018f00a7bb39a507fe769316fb259
Fati Pereira <53302048+GoldWomenAcademy@users.noreply.github.com>
2021-03-16 19:41:39 +0100

    Rules for function echo

---
+++ function_echo.php
@@ -0,0 +1,8 @@
+<?php
+/* Pas obligatoire les parenthèses avec la function echo,  fonctionne avec de simple côtes.
+Son rôle de renvoyer à l'écran qui est envoyé en paramètre par la function ici 'bonjour'
+Instruction avec simple ou double cote fonctionne 
+*/
+echo ('bonjour');
+
+?>
