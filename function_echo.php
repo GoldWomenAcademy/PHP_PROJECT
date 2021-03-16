0ab3c714f26053c371d29aac69dc529c11abe403
Fati Pereira <53302048+GoldWomenAcademy@users.noreply.github.com>
2021-03-16 19:50:58 +0100

    add anti slash in function echo

--- function_echo.php
+++ function_echo.php
@@ -2,7 +2,8 @@
 /* Pas obligatoire les parenthèses avec la function echo,  fonctionne avec de simple côtes.
 Son rôle de renvoyer à l'écran qui est envoyé en paramètre par la function ici 'bonjour'
 Instruction avec simple ou double cote fonctionne 
+Avant une apostrophe mettre un anti slash 
 */
-echo ('bonjour');
+echo 'bonjour j\'aime le php';
 
 ?>
